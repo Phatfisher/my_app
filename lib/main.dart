679ca801b4d9ce13c606527a6f0cdc33b628ec39
Ma <ma@tempesttech.com>
2022-01-03 19:14:23 -0800

    Adding in firebase analytics into proj. Also included project bundle renaming for the ios proj. to match that on firebase.

--- lib/main.dart
+++ lib/main.dart
@@ -5,12 +5,14 @@ import 'package:my_app/pages/listingScreen.dart';
 import 'pages/homeScreen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
+import 'package:firebase_analytics/firebase_analytics.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+
   runApp(const MyApp());
 }
 
@@ -20,8 +22,11 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    FirebaseAnalytics _analytics = FirebaseAnalytics.instance;
+
     return MaterialApp(
       title: 'Flutter Demo',
+      navigatorObservers: [FirebaseAnalyticsObserver(analytics: _analytics)],
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
